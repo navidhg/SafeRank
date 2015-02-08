a58e75a95bd2ad09c038c5677517ae9a53511f1d
Navid Hallajian <navidhg@gmail.com>
2015-02-08 14:48:55 +0000

    Longitude and latitude were being stored the wrong way around

--- Net/safetyserver.js
+++ Net/safetyserver.js
@@ -15,13 +15,12 @@ var client = new pg.Client(conString);
 // POST route
 app.post("/upload", function(req, res) {
   // Write out POST body
-  console.log(req.body.userid);
-  console.log(req.body.sampletime);
-  console.log(req.body.rating);
-  console.log(req.body.longitude);
-  console.log(req.body.latitude);
-  console.log(req.body.rating);
-  console.log(req.body.brightness);
+  console.log('User ID: ' + req.body.userid);
+  console.log('Sample time: ' + req.body.sampletime);
+  console.log('Rating: ' + req.body.rating);
+  console.log('Longitude: ' + req.body.longitude);
+  console.log('Latitude: ' + req.body.latitude);
+  console.log('Brightness: ' + req.body.brightness);
   res.end("success");
 
   // Connect to postgres and execute query
@@ -30,7 +29,7 @@ app.post("/upload", function(req, res) {
       return console.error("Could not connect to postgres", err);
     }
     console.log("Querying...");
-    client.query("INSERT INTO safetydetail VALUES ( ($1), ($2), ($3), ($4), ($5), ($6) )", [req.body.userid, req.body.sampletime, req.body.rating, req.body.longitude, req.body.latitude, req.body.brightness], function(err, result) {
+    client.query("INSERT INTO safetydetail VALUES ( ($1), ($2), ($3), ($4), ($5), ($6) )", [req.body.userid, req.body.sampletime, req.body.rating, req.body.latitude, req.body.longitude, req.body.brightness], function(err, result) {
       if (err) {
         return console.error("Error running query", err);
       }
